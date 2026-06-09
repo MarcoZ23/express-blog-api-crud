378a42b6e52f6a9dc4cac1f432723a3114889cc6
Marco Zollo <zollomarco23@gmail.com>
2026-06-09 20:57:59 +0200

    complete bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,14 +2,19 @@ const posts = require("../data/posts");
 
 
 const index = (req, res) => {
-    res.json(posts)
+    const tag = req.query.tag;
+    if (tag) {
+        const filteredPosts = posts.filter(post => post.tags.includes(tag));
+        return res.json(filteredPosts);
+    }
+    res.json(posts);
 }
 
 const show = (req, res) => {
     const postsId = parseInt(req.params.id);
     const thisPost = posts.find(post => post.id === postsId);
     if (!thisPost) {
-        return res.status(404).send('Post non trovato');
+        return res.status(404).json({ error: true, message: "404 Post non trovato" });
     }
     res.json(thisPost);
 }
@@ -32,7 +37,7 @@ const update = (req, res) => {
     const thisPost = posts.find(post => post.id === postsId);
 
     if (!thisPost) {
-        return res.status(404).json({ error: "true", message: "404 Post non trovato" });
+        return res.status(404).json({ error: true, message: "404 Post non trovato" });
     }
 
     thisPost.title = req.body.title || thisPost.title;
@@ -53,7 +58,7 @@ const destroy = (req, res) => {
     const postsId = parseInt(req.params.id);
     const thisPost = posts.find(post => post.id === postsId);
     if (!thisPost) {
-        return res.status(404).json({ error: "true", message: "404 Post non trovato" });
+        return res.status(404).json({ error: true, message: "404 Post non trovato" });
     }
     const index = posts.indexOf(thisPost);
     posts.splice(index, 1);

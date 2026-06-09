6860ae0558f472056c89c2817b9e028d4a3bd34c
Marco Zollo <zollomarco23@gmail.com>
2026-06-09 20:40:37 +0200

    complete milestone 4

--- controllers/postController.js
+++ controllers/postController.js
@@ -28,8 +28,20 @@ const store = (req, res) => {
 
 
 const update = (req, res) => {
-    const postsId = req.params.id;
-    res.send(`Aggiornamento del post con id: ${postsId}`);
+    const postsId = parseInt(req.params.id);
+    const thisPost = posts.find(post => post.id === postsId);
+
+    if (!thisPost) {
+        return res.status(404).json({ error: "true", message: "404 Post non trovato" });
+    }
+
+    thisPost.title = req.body.title || thisPost.title;
+    thisPost.content = req.body.content || thisPost.content;
+    thisPost.image = req.body.image || thisPost.image;
+    thisPost.tags = req.body.tags || thisPost.tags;
+
+    console.log(thisPost);
+    res.status(200).json(thisPost);
 }
 
 const modify = (req, res) => {
